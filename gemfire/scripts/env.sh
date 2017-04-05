012ce49b79d287622eebb6b79e79218d9822f166
Zac Hansen <zhansen@pivotal.io>
2017-04-05 17:10:03 -0600

    cleanup

--- gemfire/scripts/env.sh
+++ gemfire/scripts/env.sh
@@ -1,11 +1,10 @@
-
 hosts="pde-gemfire-bq1d"
 hosts="$hosts pde-gemfire-gwlg"
 
 locators="pde-gemfire-bq1d"
 locators="$locators pde-gemfire-gwlg"
 
-remote_dir=/home/gfadmin/customer-demo/gemfire
+remote_dir=/home/gfadmin/demo
 
 SSH_USER=gfadmin
 
